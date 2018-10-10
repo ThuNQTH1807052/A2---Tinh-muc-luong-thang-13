b1d7792684ab56a42d517cbe4fd4b4398b103593
quynhthu <thunqth1807052@fpt.edu.vn>
2018-10-10 19:24:10 +0700

    commit lan 1

---
+++ cmake-build-debug/CMakeFiles/tinh_luong_thang_13.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tinh_luong_thang_13.dir/main.c.o"
+  "tinh_luong_thang_13.pdb"
+  "tinh_luong_thang_13"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tinh_luong_thang_13.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int soNamKinhNghiem, soNamCongHien;
+    printf("Vui long nhap so nam kinh nghiem: ");
+    scanf("%d", &soNamKinhNghiem);
+    printf("Vui long nhap so nam lam viec: ");
+    scanf("%d", &soNamCongHien);
+
+
+
+    return 0;
+}
